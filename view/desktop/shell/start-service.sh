924a9f39e4ac181030279621deb47dfb8464bbf8
Indraraj S <indraraj001@gmail.com>
2024-06-11 21:35:32 +0530

    Update start-service.sh

--- view/desktop/shell/start-service.sh
+++ view/desktop/shell/start-service.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
-if [ -d "Jarvis-personal-assistant" ]; then
+if [ -d "Jarvis-personal-mine" ]; then
     echo "folder exists! skipping service installation steps"
 else
-    echo "Jarvis-personal-assistant directory does not exists! Downloading ..."
+    echo "Jarvis-personal-mine directory does not exists! Downloading ..."
     ./install-service.sh
-    echo "installation of Jarvis-personal-assistant service successful"
+    echo "installation of Jarvis-personal-mine service successful"
 fi
 
-cd Jarvis-personal-assistant/service/
+cd Jarvis-personal-mine/service/
 echo "preparing to execute service .."
 go run main.go &
 echo "successfully executed the service"
